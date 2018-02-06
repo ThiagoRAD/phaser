69eb040f0d139c5437446d00765d4f2c09a32a05
ThiagoRAD <thauroo@gmail.com>
2018-02-06 12:32:47 -0300

    background

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  backgroundColor: "#020202"
+  backgroundColor: "#020202",
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
 };
 const game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image("background", "assets/background.png");
+}
 
-function create() {}
+function create() {
+  const width = this.sys.game.config.width;
+  const height = this.sys.game.config.height;
+  this.add.image(width / 2, height / 2, "background");
+}
 
 function update() {}
