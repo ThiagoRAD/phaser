025a1bdc37254ebdd965a92f2e08696d9f83f776
ThiagoRAD <thauroo@gmail.com>
2019-12-13 12:26:17 -0300

    main functions

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  backgroundColor: "#002fa7"
+  backgroundColor: "#020202"
 };
 const game = new Phaser.Game(config);
+
+function preload() {}
+
+function create() {}
+
+function update() {}
