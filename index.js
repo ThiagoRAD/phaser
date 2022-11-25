6f90cf49a09c0dcd315b18f85b58c4b21db143ee
ThiagoRAD <thauroo@gmail.com>
2022-11-25 12:43:52 -0300

    input actions

--- index.js
+++ index.js
@@ -11,14 +11,37 @@ const config = {
 };
 const game = new Phaser.Game(config);
 
+let player;
+let cursors;
+
 function preload() {
   this.load.image("background", "assets/background.png");
+  this.load.image("character", "assets/ysa.gif");
+  this.load.image("gem", "assets/gem.gif");
 }
 
 function create() {
   const width = this.sys.game.config.width;
   const height = this.sys.game.config.height;
   this.add.image(width / 2, height / 2, "background");
+  cursors = this.input.keyboard.createCursorKeys();
+  player = this.add.image(width / 2, height / 2, "character");
+  this.input.on("pointerdown", (pointer) => {
+    const gem = this.add.image(pointer.x, pointer.y, "gem");
+    gem.setScale(0.5);
+  }, this);
 }
 
-function update() {}
+function update() {
+  const speed = 3;
+  if(cursors.left.isDown) {
+    player.x -= speed;
+  } else if(cursors.right.isDown) {
+    player.x += speed;
+  }
+  if(cursors.up.isDown) {
+    player.y -= speed;
+  } else if(cursors.down.isDown) {
+    player.y += speed;
+  }
+}
