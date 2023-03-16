581958791fb36d4ba8a48645f2f7bc5aad1a5f07
ThiagoRAD <thauroo@gmail.com>
2023-03-16 10:30:36 -0300

    robot

--- index_lesson1.js
+++ index_lesson1.js
@@ -11,15 +11,13 @@ const config = {
 };
 const game = new Phaser.Game(config);
 
-let robot;
+let player;
 let cursors;
 
 function preload() {
-  this.load.spritesheet("robot", "assets/robot-spritesheet.png", {
-    frameWidth: 120,
-    frameHeight: 120,
-  });  
   this.load.image("background", "assets/background.png");
+  this.load.image("character", "assets/ysa.gif");
+  this.load.image("gem", "assets/gem.gif");
 }
 
 function create() {
@@ -27,7 +25,11 @@ function create() {
   const height = this.sys.game.config.height;
   this.add.image(width / 2, height / 2, "background");
   cursors = this.input.keyboard.createCursorKeys();
-  robot = this.add.sprite(width / 2, height / 2, "robot");
+  player = this.add.image(width / 2, height / 2, "character");
+  this.input.on("pointerdown", (pointer) => {
+    const gem = this.add.image(pointer.x, pointer.y, "gem");
+    gem.setScale(0.5);
+  }, this);
 }
 
 function update() {
